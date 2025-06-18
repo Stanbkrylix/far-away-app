36514e38b1cdc9bfd6212f6d6aa694a69aefdd46
Stanley J Delva <stanleyjdelva@Stanleys-MacBook-Pro.local>
2025-06-18 07:03:39 -0400

    Form template added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
-import { useState } from "react";
+import { use, useState } from "react";
 
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
+  const [items, setItems] = useState([]);
 
-    return <></>;
+  return (
+    <>
+      <div className="app-container">
+        <div>
+          <h1 className="title-h1">Far Away</h1>
+        </div>
+        <Form />
+        <PacketList />
+        <Stats />
+      </div>
+    </>
+  );
 }
 
+function Logo() {
+  return (
+    <div>
+      <h1 className="title-h1">Far Away</h1>
+    </div>
+  );
+}
+function Form() {
+  return (
+    <div className="form-container">
+      <p>What do you need for your trip?</p>
+      <form action="submit">
+        <select name="" id="">
+          <option value=""></option>
+        </select>
+        <input type="text" name="" id="task-name" placeholder="item.." />
+        <input type="button" value="ADD" />
+      </form>
+    </div>
+  );
+}
+function PacketList() {}
+function Stats() {}
 export default App;
